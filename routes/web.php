18bba351cad4983ec6e56dad9f5c07d5a207b7b9
dennyhrfsya <dennyherfansya10@gmail.com>
2025-06-13 14:29:54 +0700

    Login with Seeder

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.dashboard');
-});
-
-Route::get('/login', function () {
     return view('pages.auth.login');
 });
